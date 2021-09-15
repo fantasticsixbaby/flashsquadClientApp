d691b44fafd544f8bca5c735678827463c138922
liliccc <xianj.chen@qq.com>
2021-09-15 16:33:53 +0800

    DestinationSearch #1

--- src/screens/DestinationSearch/index.js
+++ src/screens/DestinationSearch/index.js
@@ -1,14 +1,59 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, TextInput, SafeAreaView } from "react-native";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 
 const DestinationSearch = () => {
-    return (
-        <View>
-            <Text></Text>
-        </View>
-    )
-}
+  const [fromText, setFromText] = useState("");
+  const [destinationText, setDestinationText] = useState("");
+  const [originPlace, setOriginalPlace] = useState(null);
+  const [destinationPlace, setDestinationPlace] = useState(null);
 
-export default DestinationSearch
+  useEffect(() => {
+      if (fromText && destinationPlace) {
+          console.warn('redirect to result page');
+      }
+  }, [fromText, destinationPlace])
 
-const styles = StyleSheet.create({})
+  return (
+    <SafeAreaView>
+      <View style={styles.container}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Where from"
+          value={fromText}
+          onChangeText={setFromText}
+        ></TextInput>
+        <GooglePlacesAutocomplete
+          placeholder="Where to?"
+          styles={{textInput: styles.textInput}}
+          onPress={(data, details = null) => {
+            setDestinationPlace({data, details});
+            //console.log(data, details);
+          }}
+          fetchDetails
+          query={{
+            key: "AIzaSyB1F0Q3wRGrbJpn7_u9kDH6WmM8VZ_z8MA",
+            language: "en",
+          }}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default DestinationSearch;
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    marginTop: 25,
+    height: 1000,
+    backgroundColor: "#a0abff",
+  },
+  textInput: {
+    borderRadius: 10,
+    padding: 8,
+    backgroundColor: "#eee",
+    marginVertical: 5,
+  },
+});
--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,15 +1,13 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import HomeMap from "../../components/HomeMap";
-import HomeSearch from "../../components/HomeSearch";
-import Notifications from "../../components/Notifications";
+import DestinationSearch from "../DestinationSearch";
+
 
 const HomeScreen = () => {
   return (
     <>
-    
-    <HomeMap />
-      
+    <DestinationSearch />
     </>
   );
 };
