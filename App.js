460d5dd9f072cd5f27fcbe6e0260ec23ffc0407b
liliccc <71963984+liliccc@users.noreply.github.com>
2021-10-25 21:34:23 +0800

    add AWS Auth

--- App.js
+++ App.js
@@ -5,6 +5,10 @@ import Icon from "react-native-vector-icons/AntDesign";
 import Navigator from "./src/navigation/Navigator";
 import HomeScreen from "./src/screens/HomeScreen";
 
+import Amplify from "aws-amplify";
+import awsExports from "./src/aws-exports";
+Amplify.configure(awsExports);
+
 export default function App() {
   return (
     <>
