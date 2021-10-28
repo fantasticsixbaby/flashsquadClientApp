990a4afe60b410d43cf8857172e9acb8859185de
liliccc <71963984+liliccc@users.noreply.github.com>
2021-10-28 08:22:47 +0800

    configure bug fixed

--- App.js
+++ App.js
@@ -6,7 +6,12 @@ import { withAuthenticator } from 'aws-amplify-react-native'
 
 import Amplify from "aws-amplify";
 import config from "./src/aws-exports";
-Amplify.configure(config);
+Amplify.configure({
+  ...config,
+  Analytics: {
+    disabled: true,
+  },
+});
 
 const App = () => {
   return (
